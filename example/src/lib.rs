88c0de6cf7c77e6559507a1348c09cf3e98ded69
Dylan R. Johnston <dylan.r.johnston@gmail.com>
2024-01-19 10:09:29 +0800

    Fix prelude

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,12 +1,11 @@
 mod app;
 
 #[cfg(feature = "hydrate")]
-#[wasm_bindgen]
+#[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use app::App;
     use leptos::{mount_to_body, view};
 
-    use wasm_bindgen::prelude::wasm_bindgen;
     use web_sys::console;
 
     console_error_panic_hook::set_once();
